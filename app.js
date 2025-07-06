f4b09a590c9465416234a238235e4a527a949e19
Mariyam <farooqmaryam508@gmail.com>
2025-07-06 06:19:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+ var JhoneDoe = "Jhone Doe"
+   alert(JhoneDoe);
+
+    var age = "15 years old"
+   alert(age);
+
+    var Certifed = "Certified Mobile Application Development"
+   alert(Certifed);
+
+    var pizza= "PIZZA\nPIZZ\nPIZ\nPI\nP"
+   alert(pizza);
+
+       var email= "My email address is example@gmail.com"
+   alert(email);
+
+       var smartewaytolearnJavaScript= "I am trying to learn from the book smarter way to learn JavaScript"
+   alert(smarterwaytolearnJavaScript);
+
+   var emoji= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+   alert(emoji);
